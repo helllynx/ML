d135022a29095907b50502647cedb0d6a8f2cd1f
Yanis Vasiliadis <yvasiliadis@termt.com>
2018-03-10 09:08:15 +0300

    check lab2

--- LAB2/Code_2/another_perc.py
+++ LAB2/Code_2/another_perc.py
@@ -1,34 +1,37 @@
 from random import choice
-from numpy import array, dot, random, arange
+from numpy import array, dot, random
 import matplotlib.pyplot as plt
 
 unit_step = lambda x: 0 if x < 0 else 1
 
 training_data_or = [
-    (array([0,0,1]), 0),
-    (array([0,1,1]), 1),
-    (array([1,0,1]), 1),
-    (array([1,1,1]), 1),
+    (array([0, 0, 1]), 0),
+    (array([0, 1, 1]), 1),
+    (array([1, 0, 1]), 1),
+    (array([1, 1, 1]), 1),
 ]
 
 training_data_and = [
-    (array([0,0,1]), 0),
-    (array([0,1,1]), 0),
-    (array([1,0,1]), 0),
-    (array([1,1,1]), 1),
+    (array([0, 0, 1]), 0),
+    (array([0, 1, 1]), 0),
+    (array([1, 0, 1]), 0),
+    (array([1, 1, 1]), 1),
 ]
 
-X = array([
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]
-])
+
+
+input = array([ [0,0],
+            [0,1],
+            [1,0],
+            [1,1]
+          ])
+
+
 
 def calc(training_data):
     w = random.rand(3)
     errors = []
-    eta = 0.2
+    eta = 0.5
     n = 100
 
     for i in range(n):
@@ -43,10 +46,9 @@ def calc(training_data):
         print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
     print("WEIGHT bias: {}  w1: {}  w2:{}".format(w[0], w[1], w[2]))
 
-    # After your code
     plt.style.use('ggplot')
 
-    b, w1, w2 = w
+    w2, w1, b = w
     x = -b / w1
     y = -b / w2
 
@@ -56,33 +58,13 @@ def calc(training_data):
     line_x_coords = array([0, x])
     line_y_coords = c * line_x_coords + d
 
-    print(line_x_coords)
-    print(line_y_coords)
-    print(*X[:, 1:].T)
+    plt.plot(line_x_coords, line_y_coords)
+    plt.plot(input[1:], input[:-1], 'o')
+    plt.axis([-1, 2, -1, 2])
+    plt.show()
 
-    # plt.plot(line_x_coords, line_y_coords)
-    # plt.scatter(*X[:, 1:].T, c=[0,0,0,1], s=75)
-    # plt.show()
+
+
 
 calc(training_data_and)
-# calc(training_data_or)
-
-
-# After your code
-plt.style.use('ggplot')
-
-b, w1, w2 = w
-x = -b / w1
-y = -b / w2
-
-d = y
-c = -y / x
-
-line_x_coords = array([0, x])
-line_y_coords = c * line_x_coords + d
-
-plt.plot(line_x_coords, line_y_coords)
-plt.scatter(*input[:, 1:].T, c=target, s=75)
-plt.show()
-
-
+calc(training_data_or)
--- LAB2/Code_2/xor.py
+++ LAB2/Code_2/xor.py
@@ -1,44 +1,101 @@
-#   XOR.py-A very simple neural network to do exclusive or.
-#   sigmoid activation for hidden layer, no (or linear) activation for output
-
 import numpy as np
-import matplotlib.pyplot as plt
-
-epochs = 20000  # Number of iterations
-inputLayerSize, hiddenLayerSize, outputLayerSize = 2, 3, 1
-L = 0.1  # learning rate
-
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-Y = np.array([[0], [1], [1], [0]])
 
 
-def sigmoid(x): return 1 / (1 + np.exp(-x))  # activation function
+def leanr_xor(scheme, epochs):
+    epochs = 20000  # Number of iterations
+    inputLayerSize, hiddenLayerSize, outputLayerSize = scheme
+    L = 0.1  # learning rate
+
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    Y = np.array([[0], [1], [1], [0]])
+
+    def sigmoid(x): return 1 / (1 + np.exp(-x))  # activation function
+
+    def sigmoid_(x): return x * (1 - x)  # derivative of sigmoid
+
+    # weights on layer inputs
+    Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
+    Wz = np.random.uniform(size=(hiddenLayerSize, outputLayerSize))
+
+    for _ in range(epochs):
+        H = sigmoid(np.dot(X, Wh))  # hidden layer results
+        Z = np.dot(H, Wz)  # output layer, no activation
+        E = Y - Z  # error
+        dZ = E * L  # delta Z
+        Wz += H.T.dot(dZ)  # update output layer weights
+        dH = dZ.dot(Wz.T) * sigmoid_(H)  # delta H
+        Wh += X.T.dot(dH)  # update hidden layer weights
+
+    print('Hidden layer: \n' + str(Wh))
+    print('Output layer: \n' + str(Wz))
+
+    print('Z: \n' + str(Z))
 
 
-def sigmoid_(x): return x * (1 - x)  # derivative of sigmoid
+leanr_xor([2, 3, 1], 20000)
 
-
-# weights on layer inputs
-Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
-Wz = np.random.uniform(size=(hiddenLayerSize, outputLayerSize))
-
-for _ in range(epochs):
-    H = sigmoid(np.dot(X, Wh))  # hidden layer results
-    Z = np.dot(H, Wz)  # output layer, no activation
-    E = Y - Z  # error
-    dZ = E * L  # delta Z
-    Wz += H.T.dot(dZ)  # update output layer weights
-    dH = dZ.dot(Wz.T) * sigmoid_(H)  # delta H
-    Wh += X.T.dot(dH)  # update hidden layer weights
 #
 #
-# def plto_solve_line(w1, w2):
-#     x = np.array()
-#     y = x*w1+w2
-#     plt.plot(x, y)
-#     plt.grid()
-#     plt.show()
 
-
-print(Z)  # what have we learnt?
-
+#
+# from numpy import exp, array, random, dot
+#
+#
+# class NeuralNetwork():
+#     def __init__(self, n=3, m=1):
+#         # Create a simple neural network with one layer of n neurons at input and 1 output
+#         random.seed(1)
+#         self.synaptic_weights = {}
+#         self.num_layers = 1
+#         self.synaptic_weights[self.num_layers] = 2 * random.random((n, m)) - 1
+#         ## Assign weights randomly
+#
+#
+#
+#     # The Sigmoid function, which describes an S shaped curve.
+#     def sigmoid(self, x, derive=False):
+#         if (derive == True):
+#             return x * (1 - x)
+#         return 1 / (1 + exp(-x))
+#
+#     # Using gradient Descent to train the network
+#     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+#         for iteration in range(number_of_training_iterations):
+#             # Forward Propagation of Signal
+#             output = self.think(training_set_inputs)
+#
+#             # Calculate the error (The difference between the desired output
+#             # and the predicted output).
+#             error = (training_set_outputs - output)
+#
+#             # Backpropagation of error
+#             adjustment = dot(training_set_inputs.T, error * self.sigmoid(output, derive=True))
+#
+#             # Adjust the weights.
+#             self.synaptic_weights[self.num_layers] += adjustment
+#
+#     # Function used to calculate ouput for any given input.
+#     def think(self, inputs):
+#         return self.sigmoid(dot(inputs, self.synaptic_weights[self.num_layers]))
+#
+# if __name__ == "__main__":
+#     # Intialize a single neuron neural network with 2 neurons + 1 threshold (reated as input set to 1) and 1 layer.
+#     neural_network = NeuralNetwork()
+#
+#     print("Random starting synaptic weights: ")
+#     print(neural_network.synaptic_weights)
+#
+#     # The training set. We have 4 examples, each consisting of 2 input values
+#     # and 1 threshold input set to 1.
+#     training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+#     training_set_outputs = array([[0, 1, 1, 0]]).T
+#     # Train the neural network using a training set.
+#     # Do it 10,000 times and make small adjustments each time.
+#     neural_network.train(training_set_inputs, training_set_outputs, 10000)
+#
+#     print("New synaptic weights after training: ")
+#     print(neural_network.synaptic_weights)
+#
+#     # Test the neural network with a some input.
+#     print("Considering new situation [1, 0, 1] -> ?: ")
+# print(neural_network.think(array([1, 0, 1])))
