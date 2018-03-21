47821cdbc8fe9b2d8aca2824652665fb9a3faca2
Yanis Vasiliadis <yvasiliadis@termt.com>
2018-03-21 23:35:48 +0300

    init 4

--- LAB2/Code_2/another_perc.py
+++ LAB2/Code_2/another_perc.py
@@ -1,52 +1,67 @@
 from random import choice
-from numpy import array, dot, random
+import numpy as np
 import matplotlib.pyplot as plt
 
-unit_step = lambda x: 0 if x < 0 else 1
+
+def step_neg(x):
+    return -1 if x < 0 else 1
+
+
+def step(x):
+    return 0 if x < 0 else 1
+
 
 training_data_or = [
-    (array([0, 0, 1]), 0),
-    (array([0, 1, 1]), 1),
-    (array([1, 0, 1]), 1),
-    (array([1, 1, 1]), 1),
+    (np.array([0, 0, 1]), 0),
+    (np.array([0, 1, 1]), 1),
+    (np.array([1, 0, 1]), 1),
+    (np.array([1, 1, 1]), 1),
 ]
 
 training_data_and = [
-    (array([0, 0, 1]), 0),
-    (array([0, 1, 1]), 0),
-    (array([1, 0, 1]), 0),
-    (array([1, 1, 1]), 1),
+    (np.array([0, 0, 1]), 0),
+    (np.array([0, 1, 1]), 0),
+    (np.array([1, 0, 1]), 0),
+    (np.array([1, 1, 1]), 1),
+]
+
+input = np.array([[0, 0],
+                  [0, 1],
+                  [1, 0],
+                  [1, 1]
+                  ])
+
+input_neg = np.array([[-1, -1],
+                      [-1, 1],
+                      [1, -1],
+                      [1, 1]
+                      ])
+
+training_data_and_neg = [
+    (np.array([-1, -1, 1]), -1),
+    (np.array([-1, 1, 1]), -1),
+    (np.array([1, -1, 1]), -1),
+    (np.array([1, 1, 1]), 1),
 ]
 
 
-
-input = array([ [0,0],
-            [0,1],
-            [1,0],
-            [1,1]
-          ])
-
-
-
-def calc(training_data):
-    w = random.rand(3)
+def calc(training_data, func_act, bipolar):
+    w = np.random.rand(3)
     errors = []
-    eta = 0.5
-    n = 100
+    eta = 0.1
+    n = 1000
 
     for i in range(n):
         x, expected = choice(training_data)
-        result = dot(w, x)
-        error = expected - unit_step(result)
+        result = np.dot(w, x)
+        error = expected - func_act(result)
         errors.append(error)
         w += eta * error * x
 
     for x, _ in training_data:
-        result = dot(x, w)
-        print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+        result = np.dot(x, w)
+        print("{}: {} -> {}".format(x[:2], result, func_act(result)))
     print("WEIGHT bias: {}  w1: {}  w2:{}".format(w[0], w[1], w[2]))
-
-    plt.style.use('ggplot')
 
     w2, w1, b = w
     x = -b / w1
@@ -55,16 +70,28 @@ def calc(training_data):
     d = y
     c = -y / x
 
-    line_x_coords = array([0, x])
-    line_y_coords = c * line_x_coords + d
+    plt.style.use('ggplot')
 
-    plt.plot(line_x_coords, line_y_coords)
-    plt.plot(input[1:], input[:-1], 'o')
-    plt.axis([-1, 2, -1, 2])
+    if (bipolar):
+        line_x_coords = np.array([-0.5, x])
+        line_y_coords = c * line_x_coords + d
+        plt.plot(line_x_coords, line_y_coords)
+        plt.plot(input_neg[1:], input_neg[:-1], 'o')
+        plt.axis([-2, 2, -2, 2])
+    else:
+        line_x_coords = np.array([-1, x])
+        line_y_coords = c * line_x_coords + d
+        plt.plot(line_x_coords, line_y_coords)
+        plt.plot(input[1:], input[:-1], 'o')
+        plt.axis([-1, 2, -1, 2])
+
     plt.show()
 
 
+print('AND bipolar')
+calc(training_data_and_neg, step_neg, True)
+print('AND binar')
+calc(training_data_and, step, False)
+print('OR')
+calc(training_data_or, step, False)
 
-
-calc(training_data_and)
-calc(training_data_or)
--- Lab3/Code3/main.py
+++ Lab3/Code3/main.py
@@ -1,68 +1,69 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x): return 1 / (1 + np.exp(-x))  # activation function
 
 
-def learn(scheme, epochs, X, Y, L):
+def sigmoid_(x): return x * (1 - x)  # derivative of sigmoid
+
+
+Wh = np.array([])
+Wz = np.array([])
+E = np.array([])
+
+
+def learn(scheme, epochs, data_count, gen_data, learning_rate):
     inputLayerSize, hiddenLayerSize, outputLayerSize = scheme
 
-    def sigmoid(x): return 1 / (1 + np.exp(-x))  # activation function
-
-    def sigmoid_(x): return x * (1 - x)  # derivative of sigmoid
-
     # weights on layer inputs
+    global Wh, Wz, E
     Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
     Wz = np.random.uniform(size=(hiddenLayerSize, outputLayerSize))
 
-    for _ in range(epochs):
-        H = sigmoid(np.dot(X, Wh))  # hidden layer results
-        Z = np.dot(H, Wz)  # output layer, no activation
-        E = Y - Z  # error
-        dZ = E * L  # delta Z
-        Wz += H.T.dot(dZ)  # update output layer weights
-        dH = dZ.dot(Wz.T) * sigmoid_(H)  # delta H
-        Wh += X.T.dot(dH)  # update hidden layer weights
+    for __ in range(data_count):
+        data = gen_data()
+        X, Y = data[0].reshape(data[0].size, 1), data[1].T
 
-    print('Hidden layer: \n' + str(Wh))
-    print('Output layer: \n' + str(Wz))
+        for _ in range(epochs):
+            H = sigmoid(np.dot(X, Wh))  # hidden layer results
+            Z = np.dot(H, Wz)  # output layer, no activation
+            E = Y - Z  # error
+            dZ = E * learning_rate  # delta Z
+            Wz += H.T.dot(dZ)  # update output layer weights
+            dH = dZ.dot(Wz.T) * sigmoid_(H)  # delta H
+            Wh += X.T.dot(dH)  # update hidden layer weights
 
-    print('Z: \n' + str(Z))
+        print('\nHidden layer: \n' + str(Wh))
+        print('\nOutput layer: \n' + str(Wz))
+
+        print('\nZ: \n' + str(Z[-1:]))
+        print('\nError: \n'+str(E[-1:]))
 
 
+def check_error():
+    sum_e = np.array([])
+    for e in E:
+        sum_e = np.append(sum_e, sum(np.abs(e)))
+    x = np.arange(sum_e.size)
+    plt.plot(x, sum_e)
+    plt.title('Error graph')
+    plt.grid(True)
+    plt.show()
 
 
 def gen_data():
-    x = np.arange(0.1, 5, 0.1)
+    x = np.arange(1, 10, 0.2)
     x = x.reshape(x.size, 1)
     C = 0.9 * np.random.rand() + 0.1
     A = 0.9 * np.random.rand() + 0.1
     S = 0.9 * np.random.rand() + 0.1
     print('\nC = ' + str(C) + '\nA = ' + str(A) + '\nS = ' + str(S))
 
-    # return C * np.exp(-(np.power((x - A), 2) / S)), np.array(encl([C,A,S],x.size))
-    return C * np.exp(-(np.power((x - A), 2) / S)), np.array([C,A,S])
-
-#
-# def den_m():
-#     P = np.zeros([100, 21])
-#     T = np.zeros([3, 100])
-#     x = np.arange(0.1, 5*np.exp(-2), 1)
-#     for _ in range(0, 100):
-#         c = 0.9 * np.random.rand() + 0.1
-#         a = 0.9 * np.random.rand() + 0.1
-#         s = 0.9 * np.random.rand() + 0.1
-#         T[0, _] = c
-#         T[1, _] = a
-#         T[2, _] = s
-#         P[_,:] =c * np.exp(-(np.power((x - a),2) / s))
-#
-#     return P,T
-
-def encl(l ,n):
-    s = []
-    for i in range(0,n):
-        s.append(l)
-    return s
-
-data = gen_data()
-np.seterrcall(learn([1, 15, 3], 200, data[0].reshape(data[0].size, 1), data[1].T, 0.01))
+    return C * np.exp(-(np.power((x - A), 2) / S)), np.array([C, A, S])
 
 
+# learn([1, 15, 3], 200, data[0].reshape(data[0].size, 1), data[1].T, 0.01)
+learn([1, 1, 3], 1000, 10, gen_data, 0.01)
+
+
+check_error()
